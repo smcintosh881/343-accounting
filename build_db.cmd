5b42b476b0f0d0672d8be18a0280e713803f05cb
Bill Tarr <betarr13@gmail.com>
2017-04-26 13:40:21 -0400

    add build db script for windows

--- build_db.cmd
+++ build_db.cmd
@@ -1,2 +1,4 @@
-del enterprise.db
-sqlite3 enterprise.db < db.schema
+psql -U postgres -f setup.schema
+set PGPASSWORD=Lpb3LMDMfaBd63bY3NayRA8ukVWscx
+psql -U accounting -h 127.0.0.1 -f pschema.schema enterprise
+set PGPASSWORD=
